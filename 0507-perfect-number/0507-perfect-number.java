19087ca6bbab573f042ef793bf2a8fc2219a827d
Riya Gobade <166010013+riyagobade@users.noreply.github.com>
2026-07-19 14:54:25 +0530

    Time: 2168 ms (5.2%), Space: 42.2 MB (26.31%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int ans = 0;
+        for(int i =1; i<num; i++){
+            if(num % i == 0){
+                ans+=i;
+            }
+        }
+        return ans == num;
+    }
+}
