81284c4f6a7bc975349e257b9e9e6d4e3abf4acb
Riya Gobade <166010013+riyagobade@users.noreply.github.com>
2026-07-25 20:08:24 +0530

    Time: 3 ms (86.43%), Space: 43.4 MB (41.36%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public char getVal(char ch){
+        switch(ch){
+            case ']': return '[';
+            case '}': return '{';
+            case ')': return '(';
+            default: return ' ';
+        }
+    }
+    public boolean isValid(String s) {
+        String opening = "{([";
+        String closing = "})]";
+        Stack<Character> st = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(opening.indexOf(ch) != -1){
+                st.push(ch);
+            } else{
+                if(st.size() == 0){
+                    return false;
+                }
+                char temp = st.pop();
+                if(getVal(ch) != temp){
+                    return false;
+                }
+            }
+        }
+        return st.size() == 0;
+    }
+}
