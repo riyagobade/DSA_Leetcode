b3ffcdaf3cfc54c3c812fc276aa73a75633e5f53
Riya Gobade <166010013+riyagobade@users.noreply.github.com>
2026-08-05 10:16:05 +0530

    Time: 15 ms (23.68%), Space: 46.7 MB (9.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,16 +3,19 @@ class Solution {
        if(s.length() != t.length()){
         return false;
        }
-        int[] count = new int[26];
-        for(int i=0; i<s.length(); i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0)+1);
         }
-        for(int num:count){
-            if(num != 0){
+        for(char ch : t.toCharArray()){
+            if(!map.containsKey(ch)){
                 return false;
             }
+            map.put(ch, map.get(ch)-1);
+            if(map.get(ch) == 0){
+                map.remove(ch);
+            }
         }
-        return true;
+        return map.isEmpty();
     }
 }
