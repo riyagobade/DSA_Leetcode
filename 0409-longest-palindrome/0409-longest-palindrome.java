36fc04f7f7d9bcf3312a01a5762d78bc82d6cf4f
Riya Gobade <166010013+riyagobade@users.noreply.github.com>
2026-08-06 10:44:42 +0530

    Time: 1 ms (99.96%), Space: 43 MB (84.83%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestPalindrome(String s) {
+          int[] freq = new int[128];
+        for (char ch : s.toCharArray()) {
+            freq[ch]++;
+        }
+        int length = 0;
+        boolean hasOdd = false;
+        for (int count : freq) {
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                hasOdd = true;
+            }
+        }
+        if (hasOdd) {
+            length++;
+        }
+        return length;
+    }
+}
