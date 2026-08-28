62f42522747084a61c8ad47951e05121ac490347
Riya Gobade <166010013+riyagobade@users.noreply.github.com>
2026-08-28 19:12:49 +0530

    Time: 0 ms (100%), Space: 42.8 MB (85.04%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        int x1 = points[0][0];
+        int y1 = points[0][1];
+        int x2 = points[1][0];
+        int y2 = points[1][1];
+        int x3 = points[2][0];
+        int y3 = points[2][1];       
+        return (y2 - y1) * (x3 - x1) != 
+               (y3 - y1) * (x2 - x1);
+    }
+}
